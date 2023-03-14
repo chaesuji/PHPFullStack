f54e6f5acf141c28bcf77c10951b62aec56bed32
chaesuji <suji0251@naver.com>
2023-03-14 18:05:51 +0900

    0314_2

---
+++ DataBase/Example/2_sql_insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO employees(
+emp_no, 
+birth_date, 
+first_name, 
+last_name,
+gender,
+hire_date
+)
+VALUES (
+500000,
+NOW(),
+'suji',
+'chae',
+'F',
+NOW()
+);
+
+SELECT * FROM employees
+WHERE emp_no = 500000;
+
+-- 되돌리기
+ROLLBACK;
+
+SELECT @@autocommit;
