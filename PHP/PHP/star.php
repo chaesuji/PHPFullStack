cb3c7318ed55b72a9d8bd5b7b77be5ecb8973a0a
chaesuji <suji0251@naver.com>
2023-03-27 16:52:38 +0900

    git_test

--- PHP/PHP/star.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $num = 5;
-    for($i=0; $i<=$num; $i++){
-        for($d=0; $d<$i; $d++){
-            echo "*";
-        }
-        echo "\n";
-    }
-?>
---
+++ PHP/Training/test/star.php
@@ -0,0 +1,9 @@
+<?php
+    $num = 5;
+    for($i=0; $i<=$num; $i++){
+        for($d=0; $d<$i; $d++){
+            echo "*";
+        }
+        echo "\n";
+    }
+?>
