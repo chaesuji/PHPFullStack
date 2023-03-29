5e05503d15ece9c1cd0411cf51bbc4e0c4ccfd45
chaesuji <suji0251@naver.com>
2023-03-29 22:53:20 +0900

    lotto

--- PHP/Training/148P_test2.php
+++ PHP/Training/148P_test2.php
@@ -1,7 +1,22 @@
 <?php
-    $rand = rand(1, 45);
-    $num = ;
-    for ($i=0; $i < ; $i++) { 
-        # code...
+$num = 6;
+$i = 0;
+    for($i=0; $i<$num; $i++){
+        $rand = rand(1,45);
+        echo $rand." ";
+        $rand_check = $rand;
+        if($rand_check = $rand){
+            rand(1, 45);
+        }
     }
+
+    // while($i < $num){
+    //     $rand = rand(1,45);
+    //     echo $rand." ";
+    //     $rand_check = $rand;
+    //     if($rand_check = $rand){
+    //         rand(1,45);
+    //     }
+    //     $i++;
+    // }
 ?>
