623a537afe1a2f86fc69e5919e2a72348c03efc0
chaesuji <suji0251@naver.com>
2023-07-07 17:43:47 +0900

    vue 2-1

--- Vue/vueshop/src/assets/JS/data.js
+++ Vue/vueshop/src/assets/JS/data.js
@@ -2,7 +2,7 @@ export default [
     // {name: '신선한 양파', price: '3000', content: '# 조금 찌그러짐', count: 1, src: require('@/assets/양파손.png'), modalflg: false},
     // {name: '시들한 양파', price: '1500', content: '# 더 찌그러짐', count: 1, src: require('@/assets/양파손.png'), modalflg: false},
     // {name: '그냥 양파', price: '2000', content: '# 신선과 시들의 사이', count: 1, src: require('@/assets/양파손.png'), modalflg: false}
-    {name: '신선한 양파', price: 3000, content: '# 조금 찌그러짐', count: 1, src: require('@/assets/양파손.png')},
+    {name: '신선한 양파', price: 3000, content: '# 조금 찌그러짐', count: 1, src: require('@/assets/mang.jpg')},
     {name: '시들한 양파', price: 1500, content: '# 더 찌그러짐', count: 1, src: require('@/assets/시들한양파.jpg')},
     {name: '그냥 양파', price: 2000, content: '# 신선과 시들의 사이', count: 1, src: require('@/assets/양파손.png')},
     {name: '계란 반 개', price: 1000, content: '# 신선한듯 안 신선함', count: 1, src: require('@/assets/반계.png')}
