9bafe819da86ceceee05ddeef49bb05179c39a2a
chaesuji <suji0251@naver.com>
2023-07-10 16:27:56 +0900

    0710 vue

--- Vue/vueshop/src/App.vue
+++ Vue/vueshop/src/App.vue
@@ -1,6 +1,6 @@
 <template>
+  <!-- ! 07/06 -->
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!--  07/06 -->
   <!-- <HelloWorld msg="ㅎㅇ?"/> -->
   <!-- index.html 수정 X / vue 파일을 수정 O -->
   <!-- no 반복문 -->
@@ -13,29 +13,52 @@
     <p>{{price2}}원</p>
   </div> -->
   <!-- <hr> -->
-  <!-- component -->
+  <!-- ! 0707 component -->
   <Navi :product1="product1" :navList="navList" />
+  <!-- ! 0710 -->
+  <!-- <br> -->
+  <!-- input 태그에 입력된 값을 변수에 실시간으로 자동 저장 -->
+  <!-- <input type="text" @input="inputtest = $event.target.value;"> -->
+  <!-- v-model -->
+  <!-- v-model을 사용하면 글자가 완성되어야 모든 글자가 나옴 -->
+  <!-- <input type="text" v-model="inputtest"> -->
+  <!-- <br> -->
+  <!-- 자동 저장 변수 -->
+  <!-- <span>{{ inputtest }}</span>
+  <br> -->
+  <!-- <div class="discount" v-if="flg"> -->
+  <div class="discount">
+    <!-- <p>🔥 지금 구매 시 20% 할인 🔥</p> -->
+    <p>🔥 지금 구매 시 {{count}}% 할인 🔥</p>
+  </div>
+  <br>
+  <!-- <button @click="hookTest = !hookTest">훅 테스트</button> -->
+  <!-- ! 0707 -->
   <ProductList @openModal="modalflg=true; pnum=i" :product="product" :modalflg="modalflg" v-for="(product, i) in products" :key="i" />
-  <Modal @closeModal="modalflg = false" @plus="products[pnum].count++" @minus="products[pnum].count--" :modalflg="modalflg" :products="products" :pnum="pnum" />
-  <!--  네비 -->
+  <!-- <div class="startTransition" :class="{endTransition : modalflg}">  -->
+    <!-- modalflg가 참일 때만 클래스 적용 / 닫기 버튼을 누를 때도 다시 class 지정해줘야 함-->
+    <!-- <Modal 
+    @closeModal="modalflg = false" 
+    :modalflg="modalflg" 
+    :products="products" 
+    :pnum="pnum" />
+  </div> -->
+  <transition name="modalTransition">
+    <Modal 
+    @closeModal="modalflg = false" 
+    :modalflg="modalflg" 
+    :products="products" 
+    :pnum="pnum" />
+  </transition>
+  <!-- @plus="products[pnum].count++" 
+  @minus="products[pnum].count--"  -->
+  <!-- * 네비 -->
   <!-- <div class="nav">
     <a href="">홈</a>
     <a href="">상품</a>
     <a href="">기타</a>
   </div> -->
-
-  <!--  모달 -->
-  <!-- <template v-for="(item,i) in products" :key="i">
-    <div class="bg_black" v-if="item.modalflg">
-      <div class="bg_white" >
-        <img :src="item.src">
-        <h4>{{item.name}}</h4>
-        <p>{{item.price}}</p>
-        <p>{{item.content}}</p>
-        <button v-on:click="item.modalflg = false">X</button>
-      </div>
-    </div>
-  </template> -->
+  <!-- * 모달 -->
   <!-- <div class="bg_black" v-if="modalflg">
     <div class="bg_white" >
       <img :src="products[pnum].src">
@@ -48,24 +71,24 @@
       <button v-on:click="modalflg = false">X</button>
     </div>
   </div> -->
+  <!-- ! 0706 -->
   <!-- 배열의 index 값 출력 -->
   <!-- <h4 v-for="(name, i) in products" :key="i">{{i}}</h4> -->
   <!-- 배열의 값 출력 -->
   <!-- <h4 v-for="name in products" :key="name">{{name}}</h4>
   <h4 v-for="item in products" :key="item">{{item.name}}</h4> -->
-  <!-- <div v-for="(item,i) in products" :key="i"> -->
-    <!-- <img src={{item.src}}> -->
-    <!-- <img :src='item.src'> -->
-    <!-- <h4 v-on:click="item.modalflg = true; pnum = i;">{{item.name}}</h4> -->
-    <!-- <h4 v-on:click="openmodal(i)">{{item.name}}</h4> -->
-    <!-- <p>{{item.price}}원</p> -->
-    <!--  event -->
-    <!-- <button v-on:click="item.count++">+</button><span>{{item.count}}</span> -->
-    <!-- <button v-on:click="plus(i)">+</button><span>{{item.count}}</span>
-    <button v-on:click="minus(i)">-</button> -->
-  <!-- </div> -->
+  <!-- <div v-for="(item,i) in products" :key="i">
+    <img src={{item.src}}>
+    <img :src='item.src'>
+    <h4 v-on:click="item.modalflg = true; pnum = i;">{{item.name}}</h4>
+    <h4 v-on:click="openmodal(i)">{{item.name}}</h4>
+    <p>{{item.price}}원</p>
+    <button v-on:click="item.count++">+</button><span>{{item.count}}</span>
+    <button v-on:click="plus(i)">+</button><span>{{item.count}}</span>
+    <button v-on:click="minus(i)">-</button>
+  </div> -->
   <hr>
-  <!--  07/07 -->
+  <!-- ! 07/07 -->
   <!--  if -->
   <!-- <p v-if="1 == 1">if</p> -->
 </template>
@@ -74,7 +97,6 @@
 // import HelloWorld from './components/HelloWorld.vue'
 // * js import 
 import data from './assets/JS/data.js'
-
 import Navi from './components/Navi.vue'
 import ProductList from './components/ProductList.vue'
 import Modal from './components/Modal.vue'
@@ -84,10 +106,14 @@ export default {
   data() { // 데이터 바인딩
     return {
       // count: 1,
+      inputtest: '',
       products: data, // * js import 
       modalflg: false,
       pnum: 0,
       pprice: 0,
+      hookTest: false,
+      flg: false,
+      count: 20,
       navList: ['홈', '상품', '기타'],
       // [
       //   {name: '양파', price: '500', count: 1, src: require('@/assets/양파손.png')}, // img src 데이터바인딩
@@ -105,6 +131,27 @@ export default {
       styleR: 'color:red'
     }
   },
+  mounted() {
+    const counted = setInterval(() => {
+      this.count--
+      if(this.count == 0){
+        clearInterval(counted);
+      }
+    }, 1000);
+  },
+  // updated() {
+  //   this.flg = true;
+  // },
+  // ! 0710
+  watch: { // 실시간 감시 함수 정의 영역
+    inputtest(input) {
+      if(input == 3) {
+        alert('3333');
+        this.inputtest = '';
+      }
+    }
+  },
+  // ! 0707
   // methods: { // 함수를 설정하는 영역
   //   plus(i){
   //     this.products[i].count++;
@@ -119,6 +166,7 @@ export default {
   //     this.pnum = i;
   //   },
   // },
+  // ! 0707s
   components: {
     // HelloWorld
     Navi,
@@ -130,8 +178,9 @@ export default {
 </script>
 
 <style>
-/* * css import */
+/* 0707 css import */
 @import url('./assets/CSS/app.css');
+/* 0706 */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- Vue/vueshop/src/components/Modal.vue
+++ Vue/vueshop/src/components/Modal.vue
@@ -4,16 +4,20 @@
         <div class="bg_white">
             <img :src="products[pnum].src">
             <h4>{{products[pnum].name}}</h4>
-            <p>{{products[pnum].price * products[pnum].count}}원</p>
+            <p>{{products[pnum].price * this.onlyNumber}}원</p>
+            <!-- <p>{{products[pnum].price * products[pnum].count}}원</p> -->
             <p>{{products[pnum].content}}</p>
 
-            <button @click="$emit('plus')">+</button>
-            <span>{{products[pnum].count}}</span>
-            <button @click="$emit('minus')">-</button>
+            <input type="text" @input="onlyNumber = $event.target.value">
+            <br>
+            <span>{{ onlyNumber }}</span>
+            <!-- <button @click="$emit('plus')">+</button> -->
+            <!-- <span>{{products[pnum].count}}</span> -->
+            <!-- <button @click="$emit('minus')">-</button> -->
 
             <br><br>
 
-            <button @click="$emit('closeModal');">X</button>
+            <button @click="$emit('closeModal'); this.onlyNumber = ''">X</button>
         </div>
     </div>
 </template>
@@ -24,6 +28,20 @@ export default {
         modalflg: Boolean,
         products: Object,
         pnum: Number,
+    },
+    data() {
+        return {
+            onlyNumber: 0,
+        }
+    },
+    watch: {
+        onlyNumber(onlyNumber){
+            this.onlyNumber = this.onlyNumber.replace(/[^0-9]/g, '');
+            if(onlyNumber >= 11){
+                alert('최대 선택 가능한 수량은 10개입니다.');
+                this.onlyNumber = '10';
+            }
+        }
     }
 }
 </script>
